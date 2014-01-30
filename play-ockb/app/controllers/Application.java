53761207656ca47246e2147e1abeca61a84890f4
renzeer <rj4219@hotmail.com>
2014-01-30 11:26:41 -1000

    Setup basic template
    Added navbar that can navigate between home, submit, and explore views.

--- play-ockb/app/controllers/Application.java
+++ play-ockb/app/controllers/Application.java
@@ -8,7 +8,15 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Welcome to the home page."));
+    }
+    
+    public static Result submit() {
+        return ok(submit.render("Welcome to the submit content page"));
+    }
+    
+    public static Result explore() {
+        return ok(explore.render("Welcome to the explore content page"));
     }
 
 }
