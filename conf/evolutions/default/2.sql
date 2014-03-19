b276564319b99a42a25d53181dc01bf4a0a544aa
renzeer <rj4219@hotmail.com>
2014-03-18 23:11:45 -1000

    Updated 2.sql with table inserts

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,21 +2,21 @@
 
 # --- !Ups
 
-insert into comment (id, content) values ( 1, 'Apple Inc.');
-insert into comment (id, content) values ( 2, 'Thinking Machines');
-insert into comment (id, content) values ( 3, 'RCA');
-insert into comment (id, content) values ( 4, 'Netronics');
-insert into comment (id, content) values ( 5, 'Tandy Corporation');
-insert into comment (id, content) values ( 6, 'Commodore International');
-insert into comment (id, content) values ( 7, 'MOS Technology');
-insert into comment (id, content) values ( 8, 'Micro Instrumentation and Telemetry Systems');
-insert into comment (id, content) values ( 9, 'IMS Associates, Inc.');
-insert into comment (id, content) values ( 10, 'Digital Equipment Corporation');
-insert into comment (id, content) values ( 11, 'Lincoln Laboratory');
-insert into comment (id, content) values ( 12, 'Moore School of Electrical Engineering');
-insert into comment (id, content) values ( 13, 'IBM');
-insert into comment (id, content) values ( 14, 'Amiga Corporation');
-insert into comment (id, content) values ( 15, 'Canon');
+insert into comment (id, content) values ( 1, 'I found theses notes from Anthony Christe, a prior TA for ICS211, to be very helpful for Generics. https://github.com/anthonyjchriste/ics211f13/wiki/ArrayLists-and-Generics#wiki-generics. Cheers ');
+insert into comment (id, content) values ( 2, 'Thanks Bob! Quick question though. The notes mention that we use E or T out of convention, so does that mean we can technically use any letter to represent the Generic Type?. ');
+insert into comment (id, content) values ( 3, 'While technically you may use any letter, following convention is highly recommended for portable code. ');
+insert into comment (id, content) values ( 4, 'RTM!. ');
+insert into comment (id, content) values ( 5, 'REPLY_GOES_HERE');
+insert into comment (id, content) values ( 6, 'REPLY_GOES_HERE');
+insert into comment (id, content) values ( 7, 'REPLY_GOES_HERE');
+insert into comment (id, content) values ( 8, 'REPLY_GOES_HERE');
+insert into comment (id, content) values ( 9, 'REPLY_GOES_HERE');
+insert into comment (id, content) values ( 10, 'REPLY_GOES_HERE');
+insert into comment (id, content) values ( 11, 'REPLY_GOES_HERE');
+insert into comment (id, content) values ( 12, 'REPLY_GOES_HERE');
+insert into comment (id, content) values ( 13, 'REPLY_GOES_HERE');
+insert into comment (id, content) values ( 14, 'REPLY_GOES_HERE');
+insert into comment (id, content) values ( 15, 'REPLY_GOES_HERE');
 
 # --- !Downs
 
