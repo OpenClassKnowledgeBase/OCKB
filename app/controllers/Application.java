57394aa6112f322a9365166e862b318faab92318
Alan Ho <alangyho@gmail.com>
2014-03-28 08:50:29 -1000

    "Merged" categoryModel with master
    "Merged" categoryModel with master to include CAS login/logout
    implementation.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,16 @@
 package controllers;
 
-import java.util.List;
 import com.avaje.ebean.Ebean;
 import models.*;
 import play.data.*;
 import play.mvc.*;
 import views.html.*;
+//CAS imports
+import java.net.*;
+import java.util.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.Document;
+
 
 public class Application extends Controller {
 	static Form<PostSubmission> postForm = Form.form(PostSubmission.class);
@@ -55,4 +60,56 @@ public class Application extends Controller {
     	List<Category> categoryList = Category.findAll();
     	return ok(views.html.categories.render(categoryList));
     }
+    
+	public static Result login() throws Exception {
+		  Map<String, String[]> query = request().queryString();
+		  // service url is where you will handle validation after login
+		  // or getting the user attributes after validation
+		  String serviceURL = routes.Application.login().absoluteURL(request());
+		  serviceURL = URLEncoder.encode(serviceURL, "UTF-8");
+
+		  // no query means the user needs to login
+		  if(query.size() == 0) {
+		    // url to initiate CAS login
+		    String loginURL = CAS_LOGIN + "?service=" + serviceURL;
+		    return redirect(loginURL);
+		  } else {
+		    // after successful login from CAS, you get the ticket parameter
+		    String[] tickets = query.get("ticket");
+		    if(tickets.length > 0) {
+		      String ticket = tickets[0];
+		      // url to validate the ticket
+		      String validateURL = CAS_VALIDATE + "?service=" + serviceURL + "&ticket=" + ticket;
+		      // this junk is needed to parse the xml response from the CAS server
+		      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		      DocumentBuilder db = dbf.newDocumentBuilder();
+		      // this is where we make the actual request to validate and parse the response at the same time
+		      Document doc = db.parse(new URL(validateURL).openStream());
+		      // check for successful validation
+		      doc.getElementsByTagName("cas:serviceResponse");
+		      boolean success = doc.getElementsByTagName("cas:authenticationSuccess").getLength() > 0;
+		      if(success) {
+		        // if successful, get the username and save it into your session
+		        String username = doc.getElementsByTagName("cas:user").item(0).getTextContent();
+		        session().clear();
+		        session("username", username);
+		        //User.add(-1, username, "");
+		        return redirect(routes.Application.submit());
+		      } else {
+		        // you could redirect to the CAS login here if you want to
+		      }
+		    }
+		    return redirect(routes.Application.index());
+		  }
+		}
+	
+	public static Result logout() throws Exception {
+	  // clear your session
+	  session().clear();
+	  String serviceURL = routes.Application.index().absoluteURL(request());
+	  serviceURL = URLEncoder.encode(serviceURL, "UTF-8");
+	  // redirect to CAS logout
+	  return redirect(CAS_LOGOUT + "?service=" + serviceURL);
+	}
+	
 }
